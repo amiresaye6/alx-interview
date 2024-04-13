b56cf01fa5fcb7044c3329426e509531904bb64d
Amir Alsayed <amyralsyd367@gmail.com>
2024-04-13 17:31:50 +0200

    second version with full code

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -14,9 +14,17 @@ def pascal_triangle(n):
 
     returns: two dimentional array(pascal triangel) or [] if failed.
     """
-    triangle = [[]] * n
-    column = 0
+    triangle = [[1]]
 
 
     if n <= 0:
         return []
+
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
+
