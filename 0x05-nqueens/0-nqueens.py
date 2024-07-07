375d0a87b124af69f82b703bee92656523b06822
Amir Alsayed <amyralsyd367@gmail.com>
2024-07-07 18:49:02 +0300

    inint solution with edge casess

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+N queens
+usage: 
+>> ./0-nqueens.py 4
+>> [[0, 1], [1, 3], [2, 0], [3, 2]]
+>> [[0, 2], [1, 0], [2, 3], [3, 1]]
+"""
+import sys
+
+
+# correct number of arguments
+if (len(sys.argv) != 2):
+    print("Usage: nqueens N")
+    exit(1)
+
+# correct data type
+try:
+    n = int(sys.argv[1])
+except Exception:
+    print("N must be a number")
+    exit(1)
+
+# correct range of accepted values
+if (n <= 4):
+    print("N must be at least 4")
+    exit(1)
+
+
