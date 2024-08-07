051c4f7eb6dddff9d291a6de764bf9f241ee112a
Amir Alsayed <amyralsyd367@gmail.com>
2024-08-07 18:14:01 +0300

    inti

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""
+stuff for now
+"""
+
+
+def is_prime(number: int) -> bool:
+    """
+    stuff for now
+    """
+
+def isWinner(x, nums):
+    """
+    stuff for now
+    """
+    pass
